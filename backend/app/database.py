5d0246a31813b8a5a0846487d060fa7c58df321f
CodeFun1001 <beejasanipatil3006@gmail.com>
2026-02-05 17:42:58 +0530

    update for render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
