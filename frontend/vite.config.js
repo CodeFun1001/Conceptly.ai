589899f5f2ce132944d68dd4c52326fc1d4abc68
CodeFun1001 <beejasanipatil3006@gmail.com>
2026-02-06 17:47:24 +0530

    updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target:
+            mode === 'development'
+              ? 'http://localhost:8000'
+              : 'https://conceptly-backend.onrender.com',
+
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
